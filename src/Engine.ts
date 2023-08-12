cea671a1abb214cddee03a981b2199728f583736
Andrew Blakey <ablakey@locusrobotics.com>
2023-08-12 18:03:43 -0400

    begin trying a different approach to test performance

--- src/Engine.ts
+++ src/Engine.ts
@@ -6,45 +6,12 @@ export type Position = [number, number];
 
 type Cell = { pos: Position; type: CellName };
 
-class DoubleBuffer {
-  private imageA: ImageData;
-  private bufferA: Uint32Array;
-
-  private imageB: ImageData;
-  private bufferB: Uint32Array;
-
-  private useA = true;
-
-  constructor(w: number, h: number) {
-    this.imageA = new ImageData(w, h);
-    this.bufferA = new Uint32Array(this.imageA.data.buffer);
-
-    this.imageB = new ImageData(w, h);
-    this.bufferB = new Uint32Array(this.imageB.data.buffer);
-  }
-
-  get current() {
-    return this.useA ? this.bufferA : this.bufferB;
-  }
-
-  get other() {
-    return this.useA ? this.bufferB : this.bufferA;
-  }
-
-  get imageData() {
-    return this.useA ? this.imageA : this.imageB;
-  }
-
-  swap() {
-    this.current.set(this.other);
-    this.useA = !this.useA;
-  }
-}
-
 export class Engine {
   // Image and data.
   private ctx: CanvasRenderingContext2D;
-  private doubleBuffer: DoubleBuffer;
+  private imageData: ImageData;
+  private buffer: Uint32Array;
+  private cells: Cell[];
 
   // Simulation loop.
   private lastTime = 0;
@@ -56,20 +23,17 @@ export class Engine {
     canvas.width = SIM_SIZE;
     canvas.height = SIM_SIZE;
     this.ctx = canvas.getContext("2d")!;
+    this.imageData = this.ctx.createImageData(SIM_SIZE, SIM_SIZE);
 
-    this.doubleBuffer = new DoubleBuffer(SIM_SIZE, SIM_SIZE);
+    this.cells = [];
 
     this.resizeCanvas();
 
     // Initialize the area with walls and empty.
     for (let x = 0; x < SIM_SIZE; x++) {
       for (let y = 0; y < SIM_SIZE; y++) {
-        let value = cellDescriptions.Empty.value;
-        if (x === 0 || y === 0 || x === SIM_SIZE - 1 || y === SIM_SIZE - 1) {
-          value = cellDescriptions.Wall.value;
-        }
-        this.doubleBuffer.current[y * SIM_SIZE + x] = value;
-        this.doubleBuffer.other[y * SIM_SIZE + x] = value;
+        const isWall = x === 0 || y === 0 || x === SIM_SIZE - 1 || y === SIM_SIZE - 1;
+        this.set([x, y], isWall ? "Wall" : "Empty");
       }
     }
 
@@ -112,8 +76,7 @@ export class Engine {
       cellDescriptions[kernel.cell.type].rule(kernel, this);
     });
 
-    this.doubleBuffer.swap();
-    this.ctx.putImageData(this.doubleBuffer.imageData, 0, 0);
+    // this.ctx.putImageData(this.doubleBuffer.imageData, 0, 0);
   }
 
   forEach(callback: (kernel: Kernel) => void) {
@@ -126,11 +89,7 @@ export class Engine {
   }
 
   get(pos: Position): Cell {
-    const idx = SIM_SIZE * pos[1] + pos[0];
-    return {
-      pos,
-      type: cellValueMap[this.doubleBuffer.current[idx]] as CellName,
-    };
+    return this.cells[SIM_SIZE * pos[1] + pos[0]];
   }
 
   set(pos: Position | Cell, type: CellName) {
@@ -145,7 +104,10 @@ export class Engine {
 
     const idx = SIM_SIZE * pos[1] + pos[0];
     const value = cellDescriptions[type].value;
-    this.doubleBuffer.other[idx] = value;
+    // TODO: If `Cell` becomes more complicated, we'll have to do more work than this.
+    this.cells[idx].type = type;
+
+    this.buffer[idx] = value;
   }
 
   fillRect(pos: Position, width: number, height: number, type: CellName) {
